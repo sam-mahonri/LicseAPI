cc2405b6087933e00c9918a4a482b89e9fe66793
Sam Mahonri <sammahonri06@gmail.com>
2023-09-24 14:12:39 -0300

    .

---
+++ app/about_licse.py
@@ -0,0 +1,7 @@
+about = {
+    "codename":"Furry Sunflower",
+    "version":"0.0.1 cub",
+    "author":"Sam Mahonri",
+    "university":"IF Sertão-PE",
+    "update":"25/09/2023"
+}
--- app/resources.py
+++ app/resources.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 from flask import send_file, jsonify
 from flask_restx import Resource, Namespace
-from app import limiter
+from app import limiter, about_licse
 import os
 
 licse_status_ns = Namespace('about', __name__="status")
@@ -12,24 +12,24 @@ licse_root_ns = Namespace('/')
 class AboutLicse(Resource):
     @limiter.limit("5 per minute")
     def get(self):
-        return jsonify([load_dotenv(), find_dotenv(), os.getcwd(), os.path.isfile(find_dotenv())])
-@licse_root_ns.route('/favicon.ico')
+        return jsonify(about_licse.about)
+@licse_root_ns.route('/favicon.ico', doc=False)
 class FavIcon(Resource):
     def get(self):
         return send_file('./static/source/logos/favicon.ico')
     
-@licse_root_ns.route('/swaggerui/favicon-32x32.png')
-@licse_root_ns.route('/swaggerui/favicon-16x16.png')
+@licse_root_ns.route('/swaggerui/favicon-32x32.png', doc=False)
+@licse_root_ns.route('/swaggerui/favicon-16x16.png', doc=False)
 class FavIcon32(Resource):
     def get(self):
         return send_file('./static/source/logos/favicon.ico')
 
-@licse_root_ns.route('/swaggerui/swagger-ui.css')
+@licse_root_ns.route('/swaggerui/swagger-ui.css', doc=False)
 class SwaggerStyle(Resource):
     def get(self):
         return send_file('./static/styles/swgr.css')
 
-@licse_root_ns.route('/backgrounddev')
+@licse_root_ns.route('/backgrounddev', doc=False)
 class SwaggerBack(Resource):
     def get(self):
-        return send_file('./static/source/backgrounds/background.svg')
+        return send_file('./static/source/backgrounds/background.png')
